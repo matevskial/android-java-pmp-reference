d73d5ac09bed182503013bb9a3e661f8f0da078c
Aleksandar Matevski <matevskial@gmail.com>
2021-02-23 10:07:40 +0100

    Mirror latest changes from the repository android-kotlin-reference
    Includes:
    
    * LinearLayout example as the main activity
    * ConstraintLayout example
    * notes in README

---
+++ app/src/main/java/com/matevskial/androidjavareference/ConstraintExampleActivity.java
@@ -0,0 +1,37 @@
+package com.matevskial.androidjavareference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+/**
+ * Activity for demonstrating constraint layout
+ *
+ * Contains simple button that generates the text that the TextView displays
+ */
+public class ConstraintExampleActivity extends AppCompatActivity {
+
+    private Button generateButton;
+    private TextView displayGeneratedText;
+
+    private String[] textCollection = new String[]{"Text 1", "Text 2", "Text 3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_example);
+        generateButton = findViewById(R.id.generateButton);
+        displayGeneratedText = findViewById(R.id.displayGeneratedText);
+
+        generateButton.setOnClickListener(v -> displayGeneratedText.setText(generateText()));
+    }
+
+    private String generateText() {
+        int idx = new Random().nextInt(textCollection.length);
+        return textCollection[idx];
+    }
+}
--- app/src/main/java/com/matevskial/androidjavareference/MainActivity.java
+++ app/src/main/java/com/matevskial/androidjavareference/MainActivity.java
@@ -2,13 +2,36 @@ package com.matevskial.androidjavareference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+
+/**
+ * * This is an activity class since it extends the AppCompatActivity class
+ *
+ * * An activity is class that is responsible for drawing some user interface,
+ * dynamically modify the user interface and handle user interaction
+ *
+ * * Every activity class is tied to an xml file describing the layout of the user interface
+ */
 public class MainActivity extends AppCompatActivity {
 
+    private Button constraintExampleButton;
+
+    /**
+     * Predefined methods like this are called for the setup of the activity
+     * This method specifies which layout xml file to inflateso it can be displayed and manipulated with
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        constraintExampleButton = findViewById(R.id.constraintExampleButton);
+
+        constraintExampleButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ConstraintExampleActivity.class);
+            startActivity(intent);
+        });
     }
 }
