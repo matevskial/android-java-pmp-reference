3061d4426d024e86b265ad62f77804d22922a447
Aleksandar Matevski <matevskial@gmail.com>
2021-06-11 13:54:30 +0200

    Write code to demonstrate notifications

--- app/src/main/java/com/matevskial/androidjavapmpreference/MainActivity.java
+++ app/src/main/java/com/matevskial/androidjavapmpreference/MainActivity.java
@@ -1,8 +1,13 @@
 package com.matevskial.androidjavapmpreference;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -17,6 +22,8 @@ import android.widget.Button;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "some_channel_id";
+    private static final int NOTIFICATION_ID = 0;
     private Button constraintExampleButton;
     private Button listViewExampleButton;
     private Button recyclerViewExampleButton;
@@ -25,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private Button dialogsExampleButton;
     private Button sqliteExampleButton;
     private Button serviceExampleButton;
+    private Button notificationExampleButton;
 
     /**
      * Predefined methods like this are called for the setup of the activity
@@ -42,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         dialogsExampleButton = findViewById(R.id.dialogsExampleButton);
         sqliteExampleButton = findViewById(R.id.sqliteExampleButton);
         serviceExampleButton = findViewById(R.id.serviceExampleButton);
+        notificationExampleButton = findViewById(R.id.notificationExampleButton);
+
+        createNotificationChannel();
 
         constraintExampleButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, ConstraintExampleActivity.class);
@@ -82,5 +93,38 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, ServiceExampleActivity.class);
             startActivity(intent);
         });
+
+        notificationExampleButton.setOnClickListener(v -> {
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)
+                    .setContentTitle("Example notification")
+                    .setContentText("Notification text content")
+                    .setChannelId(CHANNEL_ID)
+                    .setStyle(new NotificationCompat.BigTextStyle()
+                            .bigText("Notification big text content"))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(NOTIFICATION_ID, builder.build());
+        });
     }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "some_chanel_name";
+            String description = "some_channel_description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if(notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+    }
+
 }
